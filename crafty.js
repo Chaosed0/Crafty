f3e8f5146c98af0571cf5738e65cc0c51173f161
Ed Lu <chaosed0@gmail.com>
2015-02-18 14:14:19 -0500

    build

--- crafty.js
+++ crafty.js
@@ -8407,6 +8407,15 @@ Crafty.c("Text", {
 
         var size = (this._textFont.size || this.defaultSize);
         this.h = 1.1 * this._getFontHeight(size);
+
+        /* Offset the MBR for text alignment*/
+        if (this._textAlign === 'left' || this._textAlign === 'start') {
+            this.offsetBoundary(0, 0, 0, 0);
+        } else if (this._textAlign === 'center') {
+            this.offsetBoundary(this.w/2, 0, -this.w/2, 0);
+        } else if (this._textAlign === 'end' || this._textAlign === 'right') {
+            this.offsetBoundary(this.w, 0, -this.w, 0);
+        }
     },
 
     // Returns the font string to use
@@ -8452,6 +8461,7 @@ Crafty.c("Text", {
      */
     textAlign: function(alignment) {
         this._textAlign = alignment;
+        this._resizeForCanvas();
         this.trigger("Invalidate");
         return this;
     },
